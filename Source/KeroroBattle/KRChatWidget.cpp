187c775709f65389967dbf57ab3b1c6cef5a845c
KIM_NA_HYEON <hjna0206@naver.com>
2025-08-20 23:30:32 +0900

    챗위젯에서 챗매니저를 통해 넘어온 정보를 통해 텍스트, 버튼 상태 수정 및 버튼을 통해 다음 챗 정보 다시 업데이트

--- Source/KeroroBattle/KRChatWidget.cpp
+++ Source/KeroroBattle/KRChatWidget.cpp
@@ -2,22 +2,143 @@
 
 
 #include "KRChatWidget.h"
+#include "KeroroPlayerController.h"
+#include "ChatManager.h"
 #include "Components/TextBlock.h"
 #include "Components/Button.h"
+
 
 void UKRChatWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+    SetVisibility(ESlateVisibility::Collapsed);
+
+	PC = Cast<AKeroroPlayerController>(GetOwningPlayer());
+
+	if (PC.IsValid() && PC->KRChatManager)
+	{
+		ChatManager = PC->KRChatManager;
+		ChatManager->OnChatUpdate.AddDynamic(this, &UKRChatWidget::UpdateChatInfo);
+	}
+
+	if (NextChatButton)
+	{
+		NextChatButton->OnClicked.AddDynamic(this, &UKRChatWidget::OnSelectNextButton);
+	}
+	if (SelectButton1)
+	{
+		SelectButton1->OnClicked.AddDynamic(this, &UKRChatWidget::OnSelectButton1);
+	}
+	if (SelectButton2)
+	{
+		SelectButton2->OnClicked.AddDynamic(this, &UKRChatWidget::OnSelectButton2);
+	}
 }
 
 void UKRChatWidget::NativeDestruct()
 {
+	if (ChatManager.IsValid())
+	{
+		ChatManager->OnChatUpdate.RemoveDynamic(this, &UKRChatWidget::UpdateChatInfo);
+	}
+	if (NextChatButton)
+	{
+		NextChatButton->OnClicked.RemoveDynamic(this, &UKRChatWidget::OnSelectNextButton);
+	}
+	if (SelectButton1)
+	{
+		SelectButton1->OnClicked.RemoveDynamic(this, &UKRChatWidget::OnSelectButton1);
+	}
+	if (SelectButton2)
+	{
+		SelectButton2->OnClicked.RemoveDynamic(this, &UKRChatWidget::OnSelectButton2);
+	}
 	Super::NativeDestruct();
 }
 
-void UKRChatWidget::ChangeText(FString str)
+void UKRChatWidget::UpdateChatInfo(int32 chatid, FString chattext, TArray<FString> SelectChatTexts, TArray<FString> nextchatids)
 {
-	if (!ChatText)return;
+    ChatID = chatid;
+    NextChatID1 = 0;
+    NextChatID2 = 0;
 
-	ChatText->SetText(FText::FromString(str));
+    if (ChatText)
+    {
+        ChatText->SetText(FText::FromString(chattext));
+    }
+
+    // 선택지/NextIDs 모두 없는 경우 위젯 숨김
+    if (SelectChatTexts.Num() == 0 && nextchatids.Num() == 0)
+    {
+        if (NextChatButton) NextChatButton->SetVisibility(ESlateVisibility::Collapsed);
+        if (SelectButton1) SelectButton1->SetVisibility(ESlateVisibility::Collapsed);
+        if (SelectButton2) SelectButton2->SetVisibility(ESlateVisibility::Collapsed);
+        if (ChatText) ChatText->SetText(FText::GetEmpty());
+
+        this->SetVisibility(ESlateVisibility::Collapsed);
+        return;
+    }
+
+    // 선택지가 있을 때
+    if (SelectChatTexts.Num() > 0 && nextchatids.Num() == SelectChatTexts.Num())
+    {
+        // 선택지1
+        if (SelectButton1 && SelectText1)
+        {
+            SelectButton1->SetVisibility(ESlateVisibility::Visible);
+            SelectText1->SetText(FText::FromString(SelectChatTexts[0]));
+            NextChatID1 = FCString::Atoi(*nextchatids[0]);
+        }
+
+        // 선택지2
+        if (SelectButton2 && SelectText2 && SelectChatTexts.Num() > 1)
+        {
+            SelectButton2->SetVisibility(ESlateVisibility::Visible);
+            SelectText2->SetText(FText::FromString(SelectChatTexts[1]));
+            NextChatID2 = FCString::Atoi(*nextchatids[1]);
+        }
+        else if (SelectButton2)
+        {
+            SelectButton2->SetVisibility(ESlateVisibility::Collapsed);
+        }
+
+        if (NextChatButton) NextChatButton->SetVisibility(ESlateVisibility::Collapsed);
+    }
+    else if (nextchatids.Num() > 0)
+    {
+        // 선택지 없고 NextID만 있을 때
+        if (NextChatButton)
+        {
+            NextChatButton->SetVisibility(ESlateVisibility::Visible);
+            NextChatID1 = FCString::Atoi(*nextchatids[0]);
+        }
+
+        if (SelectButton1) SelectButton1->SetVisibility(ESlateVisibility::Collapsed);
+        if (SelectButton2) SelectButton2->SetVisibility(ESlateVisibility::Collapsed);
+    }
+}
+
+void UKRChatWidget::OnSelectButton1()
+{
+	if (ChatManager.IsValid())
+	{
+		ChatManager->StartChat(NextChatID1);
+	}
+}
+
+void UKRChatWidget::OnSelectButton2()
+{
+	if (ChatManager.IsValid())
+	{
+		ChatManager->StartChat(NextChatID2);
+	}
+}
+
+void UKRChatWidget::OnSelectNextButton()
+{
+	if (ChatManager.IsValid())
+	{
+		ChatManager->StartChat(NextChatID1);
+	}
 }
--- Source/KeroroBattle/KRChatWidget.h
+++ Source/KeroroBattle/KRChatWidget.h
@@ -7,7 +7,7 @@
 #include "KRChatWidget.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class KEROROBATTLE_API UKRChatWidget : public UUserWidget
@@ -19,12 +19,43 @@ protected:
 	virtual void NativeDestruct() override;
 
 public:
-	void ChangeText(FString str);
+	int32 ChatID;
+	int32 NextChatID1;
+	int32 NextChatID2;
+
+public:
+	UFUNCTION()
+	void UpdateChatInfo(int32 chatid, FString chattext, TArray<FString> SelectChatTexts, TArray<FString> nextchatids);
+
+	UFUNCTION()
+	void OnSelectButton1();
+
+	UFUNCTION()
+	void OnSelectButton2();
+
+	UFUNCTION()
+	void OnSelectNextButton();
+
+private:
+	TWeakObjectPtr<class UChatManager> ChatManager;
+	TWeakObjectPtr<class AKeroroPlayerController> PC;
 
 private:
 	UPROPERTY(meta = (BindWidget))
 	class UTextBlock* ChatText;
 
 	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SelectText1;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SelectText2;
+
+	UPROPERTY(meta = (BindWidget))
 	class UButton* NextChatButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* SelectButton1;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* SelectButton2;
 };
