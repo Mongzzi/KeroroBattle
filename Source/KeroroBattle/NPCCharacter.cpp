9663032ad1fad003e007f7dfb8a4758471f1c8a9
KIM_NA_HYEON <hjna0206@naver.com>
2025-08-20 23:30:56 +0900

    NPC캐릭터 추가 작업 중

---
+++ Source/KeroroBattle/NPCCharacter.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPCCharacter.h"
+
+// Sets default values
+ANPCCharacter::ANPCCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh>SKMESH(TEXT("/Game/Keroro_Model/joriri/joriri.joriri"));
+	if (SKMESH.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(SKMESH.Object);
+	}
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FRotator(0.0f, -90.0f, 0.0f));
+}
+
+// Called when the game starts or when spawned
+void ANPCCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANPCCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ANPCCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
